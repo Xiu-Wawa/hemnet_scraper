a17f53d22ec49558811f968ad4e6230d89293c96
Xiu-Wawa <joshuaclet2@gmail.com>
2022-06-02 23:07:11 +0800

    edit

--- gumtreescraper/gumtreescraper/items.py
+++ gumtreescraper/gumtreescraper/items.py
@@ -1,11 +1,23 @@
 import scrapy
+from scrapy.loader import ItemLoader
+from itemloaders.processors import TakeFirst, MapCompose
+from w3lib.html import remove_tags
 
 
-class GumtreeItem(scrapy.Item):
-    url = scrapy.Field()
-    title = scrapy.Field()
-    price = scrapy.Field()
-    description = scrapy.Field()
-    location = scrapy.Field()
-    image_urls = scrapy.Field()
-    images = scrapy.Field()
+
+def clean_price(value):
+    return value.replace('£', '').replace('pm', '').replace('pw', '')
+
+
+def remove_space(value):
+    return value.replace('\n', '').replace('\t', '')
+
+def path(value, response):
+    return response.urljoin(value)
+
+
+class GumtreescraperItem(scrapy.Item):
+    link = scrapy.Field(input_processor=MapCompose(remove_tags, remove_space, path), output_processor=TakeFirst())
+    title = scrapy.Field(input_processor=MapCompose(remove_tags, remove_space), output_processor=TakeFirst())
+    location = scrapy.Field(input_processor=MapCompose(remove_tags, remove_space), output_processor=TakeFirst())
+    price = scrapy.Field(input_processor=MapCompose(remove_tags, clean_price), output_processor=TakeFirst())
--- gumtreescraper/gumtreescraper/spiders/gumtree.py
+++ gumtreescraper/gumtreescraper/spiders/gumtree.py
@@ -1,25 +1,31 @@
 import scrapy
-
-from ..items import GumtreeItem
+from gumtreescraper.items import GumtreescraperItem
+from scrapy.loader import ItemLoader
 
 
 class GumtreeScraper(scrapy.Spider):
 	name = "gumtree"
+	allowed_domains = ["web"]
 	start_urls = ['https://www.gumtree.com/flats-houses/london']
 
+
+	def start_request(self):
+		url = 'https://www.gumtree.com/flats-houses/london'
+		yield scrapy.Request(url=url, callback=self.parse)
+
+
 	def parse(self, response):
-		items = GumtreeItem()
+		for ad in response.xpath('//article[@class="listing-maxi"]'):
+			l = ItemLoader(item = GumtreescraperItem(), selector=ad)
 
-		ads = response.xpath('//article[@class="listing-maxi"]')
+			l.add_xpath('link', './a/@href')
+			l.add_xpath('title', './/h2[@class="listing-title"]')
+			l.add_xpath('location', './/span[@class="truncate-line"]')
+			l.add_xpath('price', './/strong[@class="h3-responsive"]')
 
-		for ad in ads:
-			items['url'] = response.urljoin(ad.xpath('./a/@href').extract_first())
-			items['title'] = ad.xpath('.//h2[@class="listing-title"]/text()').extract_first()
+			# item["link"] = response.urljoin(ad.xpath('./a/@href').get())
+			# item["title"] = ad.xpath('normalize-space(.//h2[@class="listing-title"]/text())').get()
+			# item["location"] = ad.xpath('normalize-space(.//span[@class="truncate-line"]/text())').get()
+			# item["price"] = ad.xpath('.//strong[@class="h3-responsive"]/text()').re('[.0-9]+')
 
-			yield items
-
-			
-
-
-
-			
+			yield l.load_item()
